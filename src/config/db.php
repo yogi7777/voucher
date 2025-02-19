bc7e20aade7f6083feb68fcd1b25c8e286ee0590
yogi7777 <jw@wwj.ch>
2025-02-19 20:39:53 +0100

    modified:   src/config/db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,6 +1,6 @@
 <?php
-$host = getenv('MYSQL_DATABASE');
-$db   = getenv('MYSQL_DB');
+$host = getenv('MYSQL_HOST');
+$db   = getenv('MYSQL_DATABASE');
 $user = getenv('MYSQL_USER');
 $pass = getenv('MYSQL_PASSWORD');
 $charset = 'utf8mb4';
